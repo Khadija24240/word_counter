7bf106a8c4b0e234008b1ae640feed20dba3e1a6
khadija baber <abdullahmughal24240@gmail.com>
2024-06-03 14:37:58 +0500

    first commit

---
+++ index.ts
@@ -0,0 +1,21 @@
+#!/usr/bin/env.node
+import inquirer from "inquirer";
+
+// Declare a constant "answers" and assign it to the result of inquirer.prompt function
+const answers: {
+  sentense: string;
+} = await inquirer.prompt([
+  {
+    name: "sentense",
+    type: "input",
+    message: "Enter your sentence to count the words",
+  },
+]);
+
+const words = answers.sentense.trim().split(" ");
+
+// Print the array of words to the console
+console.log(words);
+
+// Print the word count of the sentence to the console
+console.log(`Your sentence words count is ${words.length}`);
